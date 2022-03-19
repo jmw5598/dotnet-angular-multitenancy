607f17730fd8a1c8bb27de388e8e5ae7327c871f
Jason White <jmw5598@gmail.com>
2022-03-19 10:07:31 -0400

    wip: finished initial layout of login form

--- clients/projects/office/src/app/modules/authentication/authentication.module.ts
+++ clients/projects/office/src/app/modules/authentication/authentication.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header'
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 
@@ -30,11 +32,13 @@ import { LoggingOutComponent } from './pages/logging-out/logging-out.component';
     AuthenticationRoutingModule,
     ReactiveFormsModule,
     NzCheckboxModule,
+    NzFormModule,
     NzInputModule,
     NzGridModule,
     NzButtonModule,
     NzCardModule,
-    NzAlertModule
+    NzAlertModule,
+    NzPageHeaderModule
   ]
 })
 export class AuthenticationModule { }
