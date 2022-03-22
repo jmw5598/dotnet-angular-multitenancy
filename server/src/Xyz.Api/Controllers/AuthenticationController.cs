0dde2404c24d5b42cc52ecb024454722d66eb88d
Jason White <jmw5598@gmail.com>
2022-03-22 05:44:14 -0400

    wip: added services to Xyz.Api, started on authentication

---
+++ server/src/Xyz.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Xyz.Api.Controllers
+{
+    [Route("api/Auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        
+    }
+}
--- server/src/Xyz.Api/Program.cs
+++ server/src/Xyz.Api/Program.cs
@@ -1,4 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+using System.Text;
+
+using Xyz.Core.Entities;
+using Xyz.Infrastructure.Data;
+using Xyz.Multitenancy.Data;
+using Xyz.Multitenancy.Multitenancy;
+using Xyz.Multitenancy.Models;
+using Xyz.Multitenancy.Security;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -7,7 +22,82 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add configurations
+var multitenancyConfiguration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile(
+        @Directory.GetCurrentDirectory() + "/../Xyz.Multitenancy/config.json", 
+        optional: false, 
+        reloadOnChange: true
+    )
+    .Build();
+
+var tenantsConfiguration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile(
+        @Directory.GetCurrentDirectory() + "/../Xyz.Multitenancy/tenants.json", 
+        optional: false, 
+        reloadOnChange: true
+    )
+    .Build();
+
+builder.Services.Configure<MultitenancyConfiguration>(multitenancyConfiguration);
+builder.Services.Configure<TenantsConfiguration>(tenantsConfiguration);
+
+
+// Add servicse
+builder.Services.AddScoped<ITenantAccessor<Tenant>, TenantAccessor<Tenant>>();
+
+// Context for authenticating and tenant resolution
+builder.Services.AddDbContext<AuthenticationDbContext>(options =>
+    options.UseNpgsql(
+        multitenancyConfiguration.GetConnectionString("TPIMultitenancy")));
+
+// Tenant specific context for tenant database, this is dynamically set after tenant is resolved per request
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseNpgsql(
+        multitenancyConfiguration.GetConnectionString("TPIMultitenancy")));
+
+// For Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AuthenticationDbContext>()
+    .AddDefaultTokenProviders();
+
+// Adding Authentication
+builder.Services.AddAuthentication(options => {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options => {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidAudience = configuration["JWT:ValidAudience"],
+            ValidIssuer = configuration["JWT:ValidIssuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+        };
+    });
+
+// Add Policy authorizing user with tenant
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy(PolicyNames.RequireTenant,
+        policy =>
+        {
+            policy.AddRequirements(new InCurrentTenantRequirement(new HttpContextAccessor()));
+            policy.RequireAuthenticatedUser();
+        });
+});
+
+
+// Build application
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,3 +113,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+
