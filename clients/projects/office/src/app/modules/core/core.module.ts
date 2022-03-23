a3f09c2b59609f79e73a5e64ea4dcc3786202fbb
Jason White <jmw5598@gmail.com>
2022-03-23 17:04:49 -0400

    wip: created session cache service, to cache logged in user

--- clients/projects/office/src/app/modules/core/core.module.ts
+++ clients/projects/office/src/app/modules/core/core.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { JwtTokenInterceptor } from './interceptors';
+import { CACHE_SERVICE, SessionCacheService } from './services';
 
 const jwtTokenInterceptorProvider = {
   provide: HTTP_INTERCEPTORS,
   useClass: JwtTokenInterceptor,
   multi: true
+}
+
+const windowProvider = {
+  provide: Window,
+  useValue: window
+}
+
+const cacheProvider = {
+  provide: CACHE_SERVICE,
+  useExisting: SessionCacheService
 }
 
 @NgModule({
@@ -15,7 +26,9 @@ const jwtTokenInterceptorProvider = {
     CommonModule
   ],
   providers: [
-    jwtTokenInterceptorProvider
+    jwtTokenInterceptorProvider,
+    windowProvider,
+    cacheProvider
   ]
 })
 export class CoreModule { }
--- clients/projects/office/src/app/modules/core/services/index.ts
+++ clients/projects/office/src/app/modules/core/services/index.ts
@@ -1,3 +1,5 @@
 export * from './abstract-crud.service';
 export * from './crud-operations.interface';
 export * from './environment.service';
+export * from './cache-service.interface';
+export * from './session-cache.service';
