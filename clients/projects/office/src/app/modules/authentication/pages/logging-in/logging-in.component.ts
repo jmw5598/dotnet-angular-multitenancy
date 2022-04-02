66ff9d2f5b68edc852d5738bd71147d8b139c512
Jason White <jmw5598@gmail.com>
2022-04-02 12:50:10 -0400

    wip: updated logging-in page to redirect after user settings and permissions are resolved

--- clients/projects/office/src/app/modules/authentication/pages/logging-in/logging-in.component.ts
+++ clients/projects/office/src/app/modules/authentication/pages/logging-in/logging-in.component.ts
@@ -5,6 +5,7 @@ import { fadeAnimation } from '@xyz/office/modules/shared/animations';
 
 import * as fromRoot from '@xyz/office/store';
 import * as fromUser from '@xyz/office/store/user';
+import { combineLatest, delay, filter, take } from 'rxjs';
 
 @Component({
   selector: 'xyz-logging-in',
@@ -21,9 +22,24 @@ export class LoggingInComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    setTimeout(() => this._router.navigateByUrl('/'), 500);
+    this._dispatchActionsForUserSettingsAndPermissions();
+    this._listenForUserSettingsAndPermissionsResolution();
+  }
+
+  private _dispatchActionsForUserSettingsAndPermissions(): void {
     this._store.dispatch(fromUser.getUserPermissionsRequest());
     this._store.dispatch(fromUser.getUserSettingsRequest());
   }
 
+  private _listenForUserSettingsAndPermissionsResolution(): void {
+    combineLatest([
+      this._store.select(fromUser.selectUserSettings),
+      this._store.select(fromUser.selectUserPermissionsMap)
+    ])
+    .pipe(
+      filter(([settings, permissions]) => !!settings && !!permissions),
+      take(1)
+    )
+    .subscribe(([settings, permissions]) => this._router.navigateByUrl('/'));
+  }
 }
