14d101ea325d5912901ec2fa08ffcfc34b91a4dd
Jason White <jmw5598@gmail.com>
2022-04-10 11:16:46 -0400

    wip: more working towards theme, setup CSS variables for use in SCSS files

--- clients/projects/office/src/app/modules/application/modules/settings/modules/user-accounts/user-accounts.module.ts
+++ clients/projects/office/src/app/modules/application/modules/settings/modules/user-accounts/user-accounts.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
+import { XyzDatatableModule } from '@xyz/office/modules/shared/modules/datatable';
 
 import { UserAccountsRoutingModule } from './user-accounts-routing.module';
 import { UserAccountsOverviewComponent } from './pages/user-accounts-overview/user-accounts-overview.component';
-import { XyzDatatableModule } from '@xyz/office/modules/shared/modules/datatable';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzButtonModule } from 'ng-zorro-antd/button';
 import { UserAccountsCreateComponent } from './pages/user-accounts-create/user-accounts-create.component';
 import { UserAccountsUpdateComponent } from './pages/user-accounts-update/user-accounts-update.component';
-import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
-import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
     NzIconModule,
     NzButtonModule,
     NzPageHeaderModule,
-    NzBreadCrumbModule
+    NzBreadCrumbModule,
+    NzCardModule
   ]
 })
 export class UserAccountsModule { }
