f7ae9e98654af224b2abce368e7cc0c133c102cb
Jason White <jmw5598@gmail.com>
2022-04-12 05:18:38 -0400

    wip: added paged header to user accounts pages

--- clients/projects/office/src/app/modules/application/modules/settings/modules/user-accounts/pages/user-accounts-create/user-accounts-create.component.ts
+++ clients/projects/office/src/app/modules/application/modules/settings/modules/user-accounts/pages/user-accounts-create/user-accounts-create.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
+import { fadeAnimation } from '@xyz/office/modules/shared/animations';
+
 @Component({
   selector: 'xyz-user-accounts-create',
   templateUrl: './user-accounts-create.component.html',
   styleUrls: ['./user-accounts-create.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [fadeAnimation]
 })
 export class UserAccountsCreateComponent implements OnInit {
 
--- clients/projects/office/src/app/modules/application/modules/settings/modules/user-accounts/pages/user-accounts-update/user-accounts-update.component.ts
+++ clients/projects/office/src/app/modules/application/modules/settings/modules/user-accounts/pages/user-accounts-update/user-accounts-update.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
+import { fadeAnimation } from '@xyz/office/modules/shared/animations';
+
 @Component({
   selector: 'xyz-user-accounts-update',
   templateUrl: './user-accounts-update.component.html',
   styleUrls: ['./user-accounts-update.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [fadeAnimation]
 })
 export class UserAccountsUpdateComponent implements OnInit {
 
