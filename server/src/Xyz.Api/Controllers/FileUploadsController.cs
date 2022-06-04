b66a818436deead280dd3ce6e09b166dc129b9a1
Jason White <jmw5598@gmail.com>
2022-06-04 05:36:04 -0400

    wip: initial work towards user account avatar uploads, have logic for saving file working, still have to refactor and figure out how to store tenant specific files in their own respective folders, will also be looking into AWS or Azure for file storage

--- server/src/Xyz.Api/Controllers/FileUploadsController.cs
+++ server/src/Xyz.Api/Controllers/FileUploadsController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 
+using System.Net.Http.Headers;
+
 using Xyz.Core.Interfaces;
 using Xyz.Core.Models;
 using Xyz.Multitenancy.Security;
@@ -26,19 +28,33 @@ namespace Xyz.Api.Controllers
         {
             long size = avatar.Length;
 
-            
-            var filePath = Path.GetTempFileName();
-
-            using (var stream = System.IO.File.Create(filePath))
-            {
-                await avatar.CopyToAsync(stream);
-            }
-        
-
             // Process uploaded files
             // Don't rely on or trust the FileName property without validation.
-
-            return Ok(new { count = 1, size });
+            try
+            {
+                var file = Request.Form.Files[0];
+                var folderName = Path.Combine("Resources", "Images");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"') ?? "";
+                    var fullPath = Path.Combine(pathToSave, fileName);
+                    var dbPath = Path.Combine(folderName, fileName);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    return Ok(new { dbPath });
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
         }
     }
 }
---
+++ server/src/Xyz.Multitenancy/Utils/TenantCustomFolderManager.cs
@@ -0,0 +1,76 @@
+using Newtonsoft.Json.Linq;
+
+namespace Xyz.Multitenancy.Utils
+{
+    public class TenantsCustomFolderManager
+    {
+        private static readonly string s_connectionsFilePath = Directory.GetCurrentDirectory() + "/connections.json";
+
+        public static bool DoesTenantHaveCustomFolder(string tenantsDirectory, string tenantName)
+        {
+            return Directory.Exists(tenantsDirectory + tenantName);
+        }
+
+        public static void CreateTenantCustomFolder(string tenantsDirectory, string tenantName)
+        {
+            var templateFiles = Directory.EnumerateFiles(tenantsDirectory + "!", "*", SearchOption.AllDirectories);
+            var directories = Directory.GetDirectories(tenantsDirectory + "!", "*", SearchOption.AllDirectories);
+            directories.ToList().ForEach(directory => Directory.CreateDirectory(directory.Replace("!", tenantName)));
+
+            foreach (var file in templateFiles)
+            {
+                var destFileName = file.Replace("!", tenantName);
+                File.Copy(file, destFileName);
+            }
+        }
+
+        /// <summary>
+        /// Creates a custom folder for the tenant.
+        /// </summary>
+        /// <param name="tenantsDirectory"></param>
+        /// <param name="tenantName"></param>
+        /// <returns>Returns True if the Content Directory was created</returns>
+        public static bool CreateContentDirectoryIfItDoesNotExist(string tenantsDirectory, string tenantName)
+        {
+            if (DoesTenantHaveCustomFolder(tenantsDirectory, tenantName) == false)
+            {
+                CreateTenantCustomFolder(tenantsDirectory, tenantName);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void DeleteTenantsCustomFolder(string tenantsDirectory, string tenantName)
+        {
+            var files = Directory.EnumerateFiles(tenantsDirectory + tenantName, "*", SearchOption.AllDirectories);
+            files.ToList().ForEach(file => File.Delete(file));
+            var directories = Directory.GetDirectories(tenantsDirectory + tenantName, "*", SearchOption.AllDirectories).ToList();
+            // directories.Sort((x, y) => y.NumberOfSlashes().CompareTo(x.NumberOfSlashes()));
+            directories.ForEach(directory => Directory.Delete(directory));
+            Directory.Delete(tenantsDirectory + tenantName);
+        }
+
+        public static void DeleteTenantFromConfiguration(string tenantName)
+        {
+            var connectionsJObject = JObject.Parse(File.ReadAllText(s_connectionsFilePath));
+            var connectionStringToken = connectionsJObject["ConnectionStrings"][tenantName];
+
+            if (connectionStringToken != null)
+            {
+                connectionStringToken.Parent.Remove();
+                connectionsJObject.ToString();
+                File.WriteAllText(s_connectionsFilePath, connectionsJObject.ToString());
+            }
+        }
+
+        public static void CreateTenantInConfiguration(string tenantName, string connectionString)
+        {
+            var connectionsJObject = JObject.Parse(File.ReadAllText(s_connectionsFilePath));
+            connectionsJObject["ConnectionStrings"].Children().Last().AddAfterSelf(new JProperty(tenantName, connectionString));
+            File.WriteAllText(s_connectionsFilePath, connectionsJObject.ToString());
+        }
+    }
+}
